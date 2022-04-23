fd24a699b2b6116bf1452876ac72bdb1d344eddf
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-04-23 00:39:24 -0300

    commit

--- index.js
+++ index.js
@@ -1,87 +1,117 @@
 import exp from "constants";
 import express from "express";
-import path from 'path';
+import { Server } from "http";
+import path from "path";
 const app = express();
-const port = 3000
+const port = 3000;
 
-const __dirname = path.resolve(path.dirname(''));
+const __dirname = path.resolve(path.dirname(""));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
-app.use(express.urlencoded());
-
+app.use(express.static(path.join(__dirname, "public")));
 
 const pokedex = [
-    {
-        id: "001",
-        description: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-        type: "Grass",
-        name: "Bulbasaur ",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-        height: "0.7m",
-        weight: "6.9kg",
-        category: "Seed",
-        abilities: "Overgrow"
-    },
+  {
+    id: 1,
+    description:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    type: "Grass",
+    name: "Bulbasaur ",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    height: "0.7m",
+    weight: "6.9kg",
+    category: "Seed",
+    abilities: "Overgrow",
+  },
 
-    {
-        id: "004",
-        description: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-        type: "Fire",
-        name: "Charmander ",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-        height: "0.6m",
-        weight: "8.5kg",
-        category: "Lizard",
-        abilities: "Blaze"
-    },
+  {
+    id: 2,
+    description:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    type: "Fire",
+    name: "Charmander ",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    height: "0.6m",
+    weight: "8.5kg",
+    category: "Lizard",
+    abilities: "Blaze",
+  },
 
-    {
-        id: "007",
-        description: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-        type: "Water",
-        name: "Squirtle ",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-        height: "0.6m",
-        weight: "9.0kg",
-        category: "Tiny Turtle",
-        abilities: "Torrent"
-    },
+  {
+    id: 3,
+    description:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    type: "Water",
+    name: "Squirtle ",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    height: "0.6m",
+    weight: "9.0kg",
+    category: "Tiny Turtle",
+    abilities: "Torrent",
+  },
 
-    {
-        id: "025",
-        description: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
-        type: "Eletric",
-        name: "Pikachu",
-        imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-        height: "0.4m",
-        weight: "6.0kg",
-        category: "Mouse",
-        abilities: "Static"
-      },
-
-]
-
+  {
+    id: 4,
+    description:
+      "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
+    type: "Eletric",
+    name: "Pikachu",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+    height: "0.4m",
+    weight: "6.0kg",
+    category: "Mouse",
+    abilities: "Static",
+  },
+];
 
 app.get("/", (req, res) => {
-  res.render("index", {pokedex});
+  res.render("index", { pokedex });
+});
+
+app.get("/details/:id", (req, res) => {
+  let pokemon;
+  pokedex.filter((element) => {
+    if (element.id == req.params.id) {
+      pokemon = element;
+    }
+  });
+  console.log(pokemon);
+  res.render("details.ejs", {
+    pokemon,
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
 app.post("/add", (req, res) => {
-  const pokemon = req.body
-  pokemon.id = pokedex.length + 1;
-  pokedex.push(pokemon)
-  res.redirect("/")
+  let id = pokedex[pokedex.length - 1].id + 1;
+  const {
+    description,
+    type,
+    name,
+    imagem,
+    height,
+    weight,
+    category,
+    abilities,
+  } = req.body;
+  pokedex.push({
+    id: id,
+    description,
+    type,
+    name,
+    imagem,
+    height,
+    weight,
+    category,
+    abilities,
+  });
+  res.redirect("/");
 });
-
-// app.get("/", (req, res) => {
-//   res.render('datails.ejs');
-// });
-
-app.get("/", (req, res) => {
-  res.render('register.ejs');
-});
-
 
 app.listen(port, () =>
   console.log("Servidor rodando em http://localhost:3000")
