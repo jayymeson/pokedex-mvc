1aa9f1b2ccef45bb8a38641063be9fb077a0fc8c
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-04-25 21:24:54 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import { Server } from "http";
 import path from "path";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000 || "https://protected-ocean-56327.herokuapp.com/";
 
 const __dirname = path.resolve(path.dirname(""));
 
